9aecbc05e3caa277df62a6399a0f71b83a3b023d
zamarshoo <shg.ivan97@gmail.com>
2019-02-26 16:09:22 +0300

    8

---
+++ 8/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+
+    int a;
+    printf("Enter a: ");
+    scanf("%i", &a);
+
+    int b;
+    printf("Enter b: ");
+    scanf("%i", &b);
+
+    for (int i = 1; i <= b; i++) {
+        int a2 = a *  a;
+        int a3 = a2 * a;
+        printf("%i =\t%i\t%i\n", i, a2, a3);
+        a++;
+    }
+
+
+
+    return 0;
+}
