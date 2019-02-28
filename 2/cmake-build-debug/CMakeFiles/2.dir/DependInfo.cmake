e8955510076340d7077db798af7aaf808be1eec3
zamarshoo <shg.ivan97@gmail.com>
2019-02-28 19:15:09 +0300

    build start App

--- 2/cmake-build-debug/CMakeFiles/2.dir/DependInfo.cmake
+++ 2/cmake-build-debug/CMakeFiles/2.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "C:/Users/shgpro/CLionProjects/2/main.c" "C:/Users/shgpro/CLionProjects/2/cmake-build-debug/CMakeFiles/2.dir/main.c.obj"
+  "C:/Users/shgpro/CLionProjects/algorithms-in-C-lessons/2/main.c" "C:/Users/shgpro/CLionProjects/algorithms-in-C-lessons/2/cmake-build-debug/CMakeFiles/2.dir/main.c.obj"
   )
 set(CMAKE_C_COMPILER_ID "GNU")
 
