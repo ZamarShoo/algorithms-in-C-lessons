309d52db2c014ec0eb3b5a36a5b1253303316f69
zamarshoo <shg.ivan97@gmail.com>
2019-03-14 15:21:12 +0300

    3

--- 3/main.c
+++ 3/main.c
@@ -1,72 +1,93 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <malloc.h>
+#include <string.h>
+#define T char
 
-int n = 5;       // сторона доски
-int sqr = 25;  // количество клеток
-
-
-int dx[9] = {0,  2,  1, -1, -2, -2, -1, 1 ,2};
-int dy[9] = {0, -1, -2, -2,  1, -1,  2, 2, 1};
-
-char txt[80];
-int board[20][20];
-int ntry[1000];
-
-int set (int i, int x, int y)
+struct TNode
 {
+    T value;
+    struct TNode *next;
+};
+typedef struct TNode Node;
 
+struct Stack
+{
+    Node *head;
+    int size;
+    int maxSize;
+};
+struct Stack Stack;
 
-    ntry[i-1] += 1;
-    for(int k = 1; k <= n; ++k)
-    {
-        txt[0] = 0;
-        for(int j = 1; j <= n; ++j)
-            sprintf(txt, "%s%4d", txt, board[k][j]);
-        printf("%s\n", txt);
+void push(T value)
+{
+    if (Stack.size >= Stack.maxSize) {
+        printf("Error stack size");
     }
-    printf("%d\n\n", i);
-
-    int j, u, v, q1 = 0;
-
-    for(j = 1; (!q1) && (j <= 8); j++)
-    {
-        u = x + dx[j];
-        v = y + dy[j];
-        if (1 <= u && u <= n &&
-            1 <= v && v <= n &&
-            board[u][v] == 0)
-        {
-            board[u][v] = i;
-            if (i < sqr)
-            {
-                q1 = set (i + 1, u, v);
-                if (q1 == 0) board[u][v] = 0;
-            }
-            else q1 = 1;
-        }
-    }
-    return q1;
+    Node *tmp = (Node*) malloc(sizeof(Node));
+    tmp->value = value;
+    tmp->next = Stack.head;
+    Stack.head = tmp;
+    Stack.size++;
 }
 
-int main(void)
-{
-    board[1][1] = 1;
-
-    for(int k = 0; k < 1000; ++k)
-        ntry[k] = 0;
-
-    set(2, 1, 1);
-
-    for(int k = 0; k < n * n; ++k)
-        printf("%d - %d\n", k, ntry[k]);
-
-    for(int i = 1; i <= n; ++i)
+T pop() {
+    if (Stack.size == 0)
     {
-        txt[0] = 0;
-        for(int j = 1; j <= n; ++j)
-            sprintf(txt, "%s%4d", txt, board[i][j]);
-        printf("%s\n", txt);
+        printf("Stack is empty");
     }
+    Node* next = NULL;
+    T value;
+    value = Stack.head->value;
+    next = Stack.head;
+    Stack.head = Stack.head->next;
+    free(next);
+    Stack.size--;
+    return value;
+}
 
+int main(int argc, char *argv[])
+{
+    Stack.maxSize = 12;
+    Stack.head = NULL;
+    char buf[] = "()[{}]"; // строка на проверку находится здесь
+        int i = 0;
+
+        while(buf[i] != '\0'){
+            char el;
+            switch(buf[i]){
+                case '(':
+                    push(buf[i]);
+                    break;
+
+                case '[':
+                    push(buf[i]);
+                    break;
+
+                case '{':
+                    push(buf[i]);
+                    break;
+
+                case ')':
+                    el = pop();
+                    if(el != '(') push(el);
+                    break;
+
+                case ']':
+                    el = pop();
+                    if(el != '[') push(el);
+                    break;
+
+                case '}':
+                    el = pop();
+                    if(el != '{') push(el);
+                    break;
+            }
+            i++;
+        }
+    if(Stack.size != 0) {
+        printf("Do not current");
+    } else {
+        printf("All Okey");
+    }
     return 0;
 }
